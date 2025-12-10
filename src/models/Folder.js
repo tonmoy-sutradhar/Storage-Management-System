efe43c69ca52e6877c7b3df811437f8a81c75c0c
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-12-11 00:54:05 +0600

    Added file schema.

---
+++ src/models/Folder.js
@@ -0,0 +1,92 @@
+// const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
+const folderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide folder name"],
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    parentFolder: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+      default: null,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "#3b82f6", // Default blue color
+    },
+    icon: String,
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    isTrashed: {
+      type: Boolean,
+      default: false,
+    },
+    trashedAt: Date,
+    metadata: mongoose.Schema.Types.Mixed,
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Virtual for child folders
+folderSchema.virtual("childFolders", {
+  ref: "Folder",
+  localField: "_id",
+  foreignField: "parentFolder",
+});
+
+// Virtual for files in folder
+folderSchema.virtual("files", {
+  ref: "File",
+  localField: "_id",
+  foreignField: "folder",
+});
+
+// Calculate folder size
+folderSchema.virtual("size").get(async function () {
+  const File = mongoose.model("File");
+  const result = await File.aggregate([
+    { $match: { folder: this._id, isTrashed: false } },
+    { $group: { _id: null, totalSize: { $sum: "$size" } } },
+  ]);
+
+  return result.length > 0 ? result[0].totalSize : 0;
+});
+
+// Calculate item count
+folderSchema.virtual("itemCount").get(async function () {
+  const File = mongoose.model("File");
+  const Folder = mongoose.model("Folder");
+
+  const [fileCount, folderCount] = await Promise.all([
+    File.countDocuments({ folder: this._id, isTrashed: false }),
+    Folder.countDocuments({ parentFolder: this._id, isTrashed: false }),
+  ]);
+
+  return fileCount + folderCount;
+});
+
+// Indexes
+folderSchema.index({ user: 1, parentFolder: 1 });
+folderSchema.index({ user: 1, isFavorite: 1 });
+folderSchema.index({ user: 1, isTrashed: 1 });
+folderSchema.index({ user: 1, path: 1 });
+
+module.exports = mongoose.model("Folder", folderSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -1,117 +1,118 @@
-// const mongoose = require("mongoose");
-// const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 // const crypto = require("crypto");
+import crypto from "crypto";
 
-// const userSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: [true, "Please provide your name"],
-//       trim: true,
-//     },
-//     email: {
-//       type: String,
-//       required: [true, "Please provide your email"],
-//       unique: true,
-//       lowercase: true,
-//       match: [
-//         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-//         "Please provide a valid email",
-//       ],
-//     },
-//     password: {
-//       type: String,
-//       required: [true, "Please provide a password"],
-//       minlength: 6,
-//       select: false,
-//     },
-//     googleId: {
-//       type: String,
-//       sparse: true,
-//     },
-//     storageUsed: {
-//       type: Number,
-//       default: 0,
-//     },
-//     storageLimit: {
-//       type: Number,
-//       default: process.env.DEFAULT_STORAGE_LIMIT || 15728640000, // 15GB
-//     },
-//     profilePicture: {
-//       type: String,
-//       default: "default.jpg",
-//     },
-//     role: {
-//       type: String,
-//       enum: ["user", "admin"],
-//       default: "user",
-//     },
-//     isVerified: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     resetPasswordToken: String,
-//     resetPasswordExpire: Date,
-//     verifyEmailToken: String,
-//     verifyEmailExpire: Date,
-//     lastLogin: Date,
-//   },
-//   {
-//     timestamps: true,
-//     toJSON: { virtuals: true },
-//     toObject: { virtuals: true },
-//   }
-// );
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 6,
+      select: false,
+    },
+    googleId: {
+      type: String,
+      sparse: true,
+    },
+    storageUsed: {
+      type: Number,
+      default: 0,
+    },
+    storageLimit: {
+      type: Number,
+      default: process.env.DEFAULT_STORAGE_LIMIT || 15728640000, // 15GB
+    },
+    profilePicture: {
+      type: String,
+      default: "default.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    verifyEmailToken: String,
+    verifyEmailExpire: Date,
+    lastLogin: Date,
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-// // Hash password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-//   this.password = await bcrypt.hash(this.password, 12);
-//   next();
-// });
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
-// // Compare password method
-// userSchema.methods.comparePassword = async function (candidatePassword) {
-//   return await bcrypt.compare(candidatePassword, this.password);
-// };
+// Compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
-// // Generate password reset token
-// userSchema.methods.generateResetPasswordToken = function () {
-//   const resetToken = crypto.randomBytes(20).toString("hex");
+// Generate password reset token
+userSchema.methods.generateResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-//   this.resetPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(resetToken)
-//     .digest("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10 minutes
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; // 10 minutes
 
-//   return resetToken;
-// };
+  return resetToken;
+};
 
-// // Generate email verification token
-// userSchema.methods.generateEmailVerificationToken = function () {
-//   const verificationToken = crypto.randomBytes(20).toString("hex");
+// Generate email verification token
+userSchema.methods.generateEmailVerificationToken = function () {
+  const verificationToken = crypto.randomBytes(20).toString("hex");
 
-//   this.verifyEmailToken = crypto
-//     .createHash("sha256")
-//     .update(verificationToken)
-//     .digest("hex");
+  this.verifyEmailToken = crypto
+    .createHash("sha256")
+    .update(verificationToken)
+    .digest("hex");
 
-//   this.verifyEmailExpire = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
+  this.verifyEmailExpire = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
 
-//   return verificationToken;
-// };
+  return verificationToken;
+};
 
-// // Virtual for storage percentage
-// userSchema.virtual("storagePercentage").get(function () {
-//   return ((this.storageUsed / this.storageLimit) * 100).toFixed(2);
-// });
+// Virtual for storage percentage
+userSchema.virtual("storagePercentage").get(function () {
+  return ((this.storageUsed / this.storageLimit) * 100).toFixed(2);
+});
 
-// // Virtual for storage left
-// userSchema.virtual("storageLeft").get(function () {
-//   return this.storageLimit - this.storageUsed;
-// });
+// Virtual for storage left
+userSchema.virtual("storageLeft").get(function () {
+  return this.storageLimit - this.storageUsed;
+});
 
-// module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
